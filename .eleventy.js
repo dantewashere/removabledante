86fd24308061cb17a8d5ff5bab3f235f3a1977bc
Dante <dantewashere@gmail.com>
2021-06-11 17:59:25 -0400

    stuff

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.setBrowserSyncConfig({
+		files: './public/styles/**/*.css'
+	});
+    return {
+      dir: {
+        input: "src",
+        output: "public",
+      },
+    };
+  };
+
+
